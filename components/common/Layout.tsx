9930a0aae5d5fd675cd6ad067b0ddfdc963dd79c
John Kim A. Querobines <querobinesj@gmail.com>
2022-07-28 21:26:04 +0800

    feat: adds scroll to top

--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,7 +1,8 @@
 import { ContextProvider } from "@components/context";
 import styled from "@emotion/styled";
 import { Currency } from "@hooks/types";
-import { ReactNode, useState } from "react";
+import { Arrow90degUp, ArrowUp } from "@styled-icons/bootstrap";
+import { ReactNode, useEffect, useState } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -15,8 +16,30 @@ type MainProps = {
   isDark?: boolean;
 };
 
+type Scroller = {
+  isVisible: boolean;
+};
+
 const Layout = ({ articles, currencies, children }: ILayout) => {
   const [isDark, setIsDark] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const scrollEvent = () => {
+    if (window.scrollY > 0) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => scrollEvent);
+
+    return () => {
+      window.removeEventListener("scroll", () => scrollEvent);
+    };
+  }, []);
+
   return (
     <ContextProvider>
       <Navbar
@@ -26,6 +49,9 @@ const Layout = ({ articles, currencies, children }: ILayout) => {
         setIsDark={setIsDark}
       />
       <Main isDark={isDark}>{children}</Main>
+      <AccessibilityScroller isVisible={isVisible}>
+        <ArrowUp size={24} />
+      </AccessibilityScroller>
       <Footer />
     </ContextProvider>
   );
@@ -39,4 +65,20 @@ const Main = styled.main<MainProps>`
     props.isDark ? "var(--text-color)" : "var(--container-color)"};
   color: ${(props) =>
     props.isDark ? "var(--container-color)" : "var(--text-color)"};
+`;
+
+const AccessibilityScroller = styled.button<Scroller>`
+  display: ${(props) => (props.isVisible ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  width: 48px;
+  height: 48px;
+  z-index: 100;
+  background-color: var(--primary-color);
+  color: white;
+  border: none;
+  border-radius: 100%;
 `;
