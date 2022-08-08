038ed6fa4ef829715585bba02eddb9a547c41eab
John Kim A. Querobines <querobinesj@gmail.com>
2022-08-08 22:55:16 +0800

    refactor: removes 5 fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { Header, Section } from "@components/main";
 import { SEOHeader } from "@components/seo";
-import { getArticlesOrdered } from "@hooks/article";
+import { getArticles } from "@hooks/article";
 import { getCurrency } from "@hooks/thirdpartyApi";
 import { InferGetStaticPropsType } from "next";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 
 export async function getStaticProps() {
-  const articles = await getArticlesOrdered();
+  const articles = await getArticles();
   const currencies = await getCurrency();
 
   return {
@@ -24,11 +24,9 @@ const Home = ({ articles }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <SEOHeader canonical="/" />
-      <Header articles={articles?.allArticles} />
+      <Header articles={articles} />
       <Section
-        cards={
-          articles?.perCategory.find((d) => d.category === "World")?.articles
-        }
+        cards={articles?.filter((d) => d.category === "World")}
         variant="quaternary"
         title="World"
         slug="/world"
@@ -40,37 +38,29 @@ const Home = ({ articles }: InferGetStaticPropsType<typeof getStaticProps>) => {
         />
       </Section>
       <Section
-        cards={
-          articles?.perCategory.find((d) => d.category === "Business")?.articles
-        }
+        cards={articles?.filter((d) => d.category === "Business")}
         variant="tertiary"
         position="left"
         title="Business"
         slug="/business"
       />
       <Section
-        cards={
-          articles?.perCategory.find(
-            (d) => d.category === "Science" || d.category === "Technology"
-          )?.articles
-        }
+        cards={articles?.filter(
+          (d) => d.category === "Science" || d.category === "Technology"
+        )}
         variant="tertiary"
         title="Tech & Science"
         slug="/tech-and-science"
       ></Section>
 
       <Section
-        cards={
-          articles?.perCategory.find((d) => d.category === "Health")?.articles
-        }
+        cards={articles?.filter((d) => d.category === "Health")}
         variant="tertiary"
         title="Health"
         slug="/health"
       ></Section>
       <Section
-        cards={
-          articles?.perCategory.find((d) => d.category === "Sports")?.articles
-        }
+        cards={articles?.filter((d) => d.category === "Sports")}
         variant="tertiary"
         title="Sports"
         slug="/sports"
