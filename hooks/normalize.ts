e714fa94fc49ce5e9ea86472908e52ab6e4932ca
John Kim A. Querobines <querobinesj@gmail.com>
2022-08-11 22:51:50 +0800

    refactor: changes normalize

--- hooks/normalize.ts
+++ hooks/normalize.ts
@@ -1,23 +1,16 @@
-import { Article } from "@components/types";
-import { DocumentData, QuerySnapshot } from "firebase/firestore";
+import { Article } from "@hooks/types";
 
-export const normalizeArticle = (snapshot: QuerySnapshot<DocumentData>) => {
-  return snapshot.docs.map(
-    (doc) =>
-      ({
-        id: doc.id ?? "",
-        createdAt: doc.data()?.createdAt?.toDate().toISOString() ?? "",
-        author: doc.data()?.author ?? "",
-        title: doc.data()?.title ?? "",
-        category: doc.data()?.category ?? "",
-        body: doc.data()?.body ?? "",
-        excerpt: doc.data()?.excerpt ?? "",
-        featuredImage: doc.data()?.featuredImage ?? "",
-        slug: doc.data()?.slug ?? "",
-      } as Article)
-  );
-};
-
-export const generateSlug = (title: string) => {
-  return title.toLowerCase().replace(/[^\w-]+/g, "-");
+export const normalizeArticle = (article: Article[]) => {
+  return article.map((item) => {
+    return {
+      ...item,
+      featuredImage: {
+        url: item.featuredImage[0].url,
+        width: item.featuredImage[0].width,
+        height: item.featuredImage[0].height,
+      },
+      category: item.category.name,
+      createdAt: item.sys.publishedAt,
+    };
+  });
 };
