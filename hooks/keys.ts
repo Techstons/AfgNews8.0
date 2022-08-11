1ec28d386e1ade599af150949f355956b4fdad0d
John Kim <querobinesj@gmail.com>
2022-08-11 18:06:25 +0800

    feat: adds graphql url

--- hooks/keys.ts
+++ hooks/keys.ts
@@ -1,2 +1,4 @@
-export const API_URL = "https://cdn.contentful.com";
+export const API_URL =
+  "https://graphql.contentful.com/content/v1/spaces/" +
+  process.env.z51ycfudg1jt;
 export const currency_access_tokens = process.env.CURRENCY_ACCESS_TOKEN;
