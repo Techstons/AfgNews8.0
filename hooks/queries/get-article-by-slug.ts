505baa47a01eed12583e43fd2aa809a89ecb4615
John Kim <querobinesj@gmail.com>
2022-08-11 18:02:32 +0800

    feat: get article by slug query

---
+++ hooks/queries/get-article-by-slug.ts
@@ -0,0 +1,15 @@
+const getArticleBySlug = `
+    query getArticleBySlug($slug: String!) {
+        articleCollection (where: { slug: $slug }, limit: 1) {
+                items {
+                title
+                category {
+                name
+                        }
+                
+                }
+        }
+    }
+`;
+
+export default getArticleBySlug;
