360600854753e3fd20e91cf98d580cdccb0f77d6
John Kim <querobinesj@gmail.com>
2022-08-17 18:21:41 +0800

    addhoc: remove unused vars

--- components/widgets/LocaleSwitcher.tsx
+++ components/widgets/LocaleSwitcher.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function LocaleSwitcher() {
   const router = useRouter();
 
-  const { locales, locale: activeLocale, pathname, query, asPath } = router;
+  const { locales, pathname, query, asPath } = router;
 
   const languages = {
     "en-US": {
@@ -24,7 +24,6 @@ export default function LocaleSwitcher() {
   return (
     <>
       {locales?.map((locale) => {
-        console.log(locale);
         return (
           <span key={"locale-" + locale}>
             <Link href={{ pathname, query }} as={asPath} locale={locale}>
