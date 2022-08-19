7e0af9430b4a80917dff53ce69709e4a4fb2005c
John Kim A. Querobines <querobinesj@gmail.com>
2022-08-19 22:04:16 +0800

    refactor: changes query to slug

--- hooks/utils/queries/get-articles-ctx.ts
+++ hooks/utils/queries/get-articles-ctx.ts
@@ -5,19 +5,19 @@ query getArticlesCtxQuery($locale: String) {
   Home:articleCollection(order: sys_publishedAt_DESC, limit: 11, locale: $locale)  {
       ${cardArticleFields}
   }
-  World:articleCollection(order: sys_publishedAt_DESC, where: {category: {name: "World"}}, limit: 4, locale: $locale)  {
+  World:articleCollection(order: sys_publishedAt_DESC, where: {category: {slug: "world"}}, limit: 4, locale: $locale)  {
       ${cardArticleFields}
   }
-  Business:articleCollection(order: sys_publishedAt_DESC, where: {category: {name: "Business"}}, limit: 4, locale: $locale)  {
+  Business:articleCollection(order: sys_publishedAt_DESC, where: {category: {slug: "business"}}, limit: 4, locale: $locale)  {
       ${cardArticleFields}
   }
-  Tech:articleCollection(order: sys_publishedAt_DESC, where: {category: {name: "Tech & Science"}}, limit: 4, locale: $locale) {
+  Tech:articleCollection(order: sys_publishedAt_DESC, where: {category: {slug: "tech-and-science"}}, limit: 4, locale: $locale) {
       ${cardArticleFields}
   }
-  Health:articleCollection(order: sys_publishedAt_DESC, where: {category: {name: "Health"}}, limit: 4, locale: $locale) {
+  Health:articleCollection(order: sys_publishedAt_DESC, where: {category: {slug: "health"}}, limit: 4, locale: $locale) {
       ${cardArticleFields}
   }
-  Sports:articleCollection(order: sys_publishedAt_DESC, where: {category: {name: "Sports"}}, limit: 4, locale: $locale) {
+  Sports:articleCollection(order: sys_publishedAt_DESC, where: {category: {slug: "sports"}}, limit: 4, locale: $locale) {
       ${cardArticleFields}
   }
 }
