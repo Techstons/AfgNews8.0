c6db3de4ac79b7d96d4bae8e571dd1ebfba18a9e
John Kim <querobinesj@gmail.com>
2022-08-24 20:36:12 +0800

    feat: change locale based on locale

--- hooks/useFormattedDate.tsx
+++ hooks/useFormattedDate.tsx
@@ -1,13 +1,22 @@
 import { format, formatDistanceToNow } from "date-fns";
+import { enUS, faIR } from "date-fns/locale";
+
 import { useState, useEffect } from "react";
 
 type FormatedDateProps = "nav" | "footer" | "widget" | "distance";
 
-const useFormattedDate = (date: Date, type: FormatedDateProps) => {
+const useFormattedDate = (
+  date: Date,
+  type: FormatedDateProps,
+  locale?: string
+) => {
   const [formattedDate, setFormattedDate] = useState<string>();
 
   useEffect(() => {
-    if (type === "nav") setFormattedDate(format(date, "E, d MMM"));
+    if (type === "nav")
+      setFormattedDate(
+        format(date, "E, do MMM", { locale: locale === "en" ? enUS : faIR })
+      );
 
     if (type === "widget") setFormattedDate(format(date, "MMM, do"));
 
