2bd3314358a166e10ef7dd2d599cd17aee336308
John Kim <querobinesj@gmail.com>
2022-08-25 18:26:58 +0800

    feat: filter out current locale

--- components/widgets/LocaleSwitcher.tsx
+++ components/widgets/LocaleSwitcher.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/router";
 export default function LocaleSwitcher() {
   const router = useRouter();
 
-  const { locales, pathname, query, asPath } = router;
+  const { locales, pathname, query, asPath, locale } = router;
 
   const languages = {
     en: {
@@ -23,15 +23,17 @@ export default function LocaleSwitcher() {
 
   return (
     <>
-      {locales?.map((locale) => {
-        return (
-          <span key={"locale-" + locale}>
-            <Link href={{ pathname, query }} as={asPath} locale={locale}>
-              <a>{languages[locale as keyof typeof languages].title}</a>
-            </Link>
-          </span>
-        );
-      })}
+      {locales
+        ?.filter((l) => l !== locale) // Filter out the current locale
+        .map((locale) => {
+          return (
+            <span key={"locale-" + locale}>
+              <Link href={{ pathname, query }} as={asPath} locale={locale}>
+                <a>{languages[locale as keyof typeof languages].title}</a>
+              </Link>
+            </span>
+          );
+        })}
     </>
   );
 }
