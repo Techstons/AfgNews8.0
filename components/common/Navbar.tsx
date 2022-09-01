acfbb78b521c1007d9c3df567d564c38f4339803
John Kim <querobinesj@gmail.com>
2022-09-01 17:06:54 +0800

    style: reduce distance between menu item and line

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -237,7 +237,6 @@ const Root = styled.nav<ToggleProps>`
     display: block; /* This will put the pseudo element on its own line. */
     margin: 0 auto; /* This will center the border. */
     width: 100%; /* Change this to whatever width you want. */
-    padding-top: 20px; /* This creates some space between the element and the border. */
     border-bottom: 1px solid black; /* This creates the border. Replace black with whatever color you want. */
   }
 
