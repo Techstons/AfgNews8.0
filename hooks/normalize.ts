0688fb2d062cbae623a1cd7c18e42de32d1cb3c0
John Kim A. Querobines <querobinesj@gmail.com>
2024-10-02 13:50:04 +0800

    fix: featured images null values

--- hooks/normalize.ts
+++ hooks/normalize.ts
@@ -1,19 +1,20 @@
-import { Article } from "@hooks/types";
+import { Article } from '@hooks/types';
 
 export const normalizeArticle = (article?: Article[]) => {
+  console.log(article);
   return !!article && article?.length !== 0
     ? article.map((item) => {
         return {
           ...item,
           featuredImage: {
-            url: item.featuredImage[0].secure_url,
-            width: item.featuredImage[0].width,
-            height: item.featuredImage[0].height,
-            id: item.featuredImage[0].public_id,
+            url: item?.featuredImage?.[0]?.secure_url || null,
+            width: item?.featuredImage?.[0]?.width || null,
+            height: item?.featuredImage?.[0]?.height || null,
+            id: item?.featuredImage?.[0]?.public_id || null,
           },
           category: item.category.name,
-          createdAt: item?.sys?.publishedAt ?? "",
-          author: item?.author?.name ?? "",
+          createdAt: item?.sys?.publishedAt ?? '',
+          author: item?.author?.name ?? '',
         };
       })
     : [];
