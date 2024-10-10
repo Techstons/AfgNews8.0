c238d749ed0493cf487e214d254b8c8cc6862d50
TecStones <58137069+Techstons@users.noreply.github.com>
2024-10-10 04:53:20 -0400

    Update get-all-articles.ts

--- hooks/article/get-all-articles.ts
+++ hooks/article/get-all-articles.ts
@@ -4,7 +4,7 @@ const spaceId = process.env.SPACE_ID;
 
 const client = createClient({
   space: spaceId ? spaceId : "",
-  accessToken: "IS_9497XlMwHMjP64Riv3m01m5n5vSB4589fPL7f7gI",
+  accessToken: "",
 });
 
 export async function getContentfulData() {
